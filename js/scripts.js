cf3fe344fe809872746c7e4f27ff5b7efb0b572b
RaTeNo <rateno@mail.ru>
2020-10-20 11:13:20 +0300

    new_vlock

--- js/scripts.js
+++ js/scripts.js
@@ -8,4 +8,12 @@ $(function() {
         }    
     });
 
+    $("input[type='radio']").change(function() {
+    	console.log(123);
+    	$("input[type='radio']").parent().removeClass("active");
+        if(this.checked) {
+            $(this).parent().addClass("active");
+        }        
+    });
+
 })
